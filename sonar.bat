f4868921a2a7492ee10bb1d1bae5869763d2665a
Andrés <andresdt@pm.me>
2021-03-11 21:16:02 +0100

    feat: add Sonarcloud script

---
+++ sonar.bat
@@ -0,0 +1,2 @@
+setx "SONAR_TOKEN" "a1c85ec2d2bdc309a67ea3fca11ad382ab2f56a5"
+mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dmaven.test.failure.ignore=true -Djacoco.haltonfailure=false
